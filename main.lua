2c2d911831a9b062a39a540e82e011e724690da3
free0ne <korianets@gmail.com>
2018-11-25 16:23:16 +0300

    test1

--- main.lua
+++ main.lua
@@ -1,8 +1,13 @@
------------------------------------------------------------------------------------------
---
--- main.lua
---
------------------------------------------------------------------------------------------
+--local bg = display.newImage("pics/bg2.jpg")
+--bg.x = display.contentCenterX
+--bg.y = display.contentCenterY
 
--- Your code here
---hello
+--display.setStatusBar(display.HiddenStatusBar)
+
+display.setDefault( "background", 12/255, 110/255, 30/255 )
+display.setDefault( "fillColor", 0, 0, 0 )
+
+widthR = display.contentWidth - 60
+
+rect1 = display.newRoundedRect( display.contentCenterX, 150, widthR, 80, 5 )
+rect1:setFillColor(50/255, 0.7)
